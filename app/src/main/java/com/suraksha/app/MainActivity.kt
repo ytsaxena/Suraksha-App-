ee406ff88fd2e15c98f89fa83e323aaab0070092
Akshita Tiwary <akshitatiwary16@gmail.com>
2025-11-06 14:07:51 +0530

    create project structure

--- app/src/main/java/com/suraksha/app/MainActivity.kt
+++ app/src/main/java/com/suraksha/app/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.suraksha.app.ui.theme.SurakshainTheme
+import com.suraksha.app.presentation.theme.SurakshainTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
