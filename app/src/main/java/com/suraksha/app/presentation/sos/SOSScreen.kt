862ac5d883fbc6f31a41053b19d29752e868ad95
Akshita Tiwary <akshitatiwary16@gmail.com>
2025-11-06 15:00:06 +0530

    add screens

---
+++ app/src/main/java/com/suraksha/app/presentation/sos/SOSScreen.kt
@@ -0,0 +1,21 @@
+package com.suraksha.app.presentation.sos
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SOSScreen(modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("SOS Screen")
+    }
+}
