a922aade0e3fc77aa33b728a3e41dc136c7b3205
Akshita Tiwary <akshitatiwary16@gmail.com>
2025-11-06 15:00:26 +0530

    apply bottom bar navigation

--- app/src/main/java/com/suraksha/app/MainActivity.kt
+++ app/src/main/java/com/suraksha/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.suraksha.app.presentation.navigaiton.App
 import com.suraksha.app.presentation.theme.SurakshainTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SurakshainTheme {
-
+                App()
             }
         }
     }
